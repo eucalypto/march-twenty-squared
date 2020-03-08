db19def095012883e5dc31420a5f27c198277e26
Viktor Papara <eucalyptov@gmail.com>
2020-03-08 19:42:13 +0100

    Implement Counter and increment Button

--- app/src/main/java/de/eucalypto/marchtwentysquared/MainActivity.kt
+++ app/src/main/java/de/eucalypto/marchtwentysquared/MainActivity.kt
@@ -1,12 +1,23 @@
 package de.eucalypto.marchtwentysquared
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    var counter = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        counterView.text = counter.toString()
+
+        incrementButton.setOnClickListener {
+            counter += 1
+            counterView.text = counter.toString()
+        }
+
     }
 }
