1fa1466d5bd6486b33dd32da7abc6144679529f9
Viktor Papara <eucalyptov@gmail.com>
2020-03-10 00:02:30 +0100

    Implement Android Jetpack's Navigation component
    Right now, this is kind of useless because there is only one fragment.

--- app/src/main/java/de/eucalypto/marchtwentysquared/MainActivity.kt
+++ app/src/main/java/de/eucalypto/marchtwentysquared/MainActivity.kt
@@ -8,10 +8,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val fragment = CountFragment()
-
-        supportFragmentManager.beginTransaction().add(R.id.mainActivity, fragment).commit()
     }
 
 }
