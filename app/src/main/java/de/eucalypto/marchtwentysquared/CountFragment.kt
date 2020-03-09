9967eaee7290688d9f052ac89c8c25c7e08a62bd
Viktor Papara <eucalyptov@gmail.com>
2020-03-09 21:52:08 +0100

    Extract counting "activity" to separate fragment

---
+++ app/src/main/java/de/eucalypto/marchtwentysquared/CountFragment.kt
@@ -0,0 +1,44 @@
+package de.eucalypto.marchtwentysquared
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_count.*
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class CountFragment : Fragment() {
+
+    var counter = 0
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_count, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        counterView.text = counter.toString()
+
+        incrementButton.setOnClickListener {
+            counter += 1
+            counterView.text = counter.toString()
+        }
+
+        resetButton.setOnClickListener {
+            counter = 0
+            counterView.text = counter.toString()
+        }
+
+
+    }
+
+}
--- app/src/main/java/de/eucalypto/marchtwentysquared/MainActivity.kt
+++ app/src/main/java/de/eucalypto/marchtwentysquared/MainActivity.kt
@@ -2,27 +2,16 @@ package de.eucalypto.marchtwentysquared
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
-    var counter = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        counterView.text = counter.toString()
+        val fragment = CountFragment()
 
-        incrementButton.setOnClickListener {
-            counter += 1
-            counterView.text = counter.toString()
-        }
-
-        resetButton.setOnClickListener {
-            counter = 0
-            counterView.text = counter.toString()
-        }
-
+        supportFragmentManager.beginTransaction().add(R.id.mainActivity, fragment).commit()
     }
+
 }
